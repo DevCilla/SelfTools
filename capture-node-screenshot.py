9f866a6b896af847543255059e520331a7b36470
Cilla-ProjectNB <cilla@project.nb.com>
2023-03-09 18:50:39 +0800

    update filename pattern; add html evaluate;

--- capture-node-screenshot.py
+++ capture-node-screenshot.py
@@ -15,14 +15,31 @@ def run(playwright):
 def browse_and_capture(context, links):
     cnt = 0
     for i in links:
-        print('URL: ' + i)
-        page = context.new_page()
-        page.goto(i)
         now = "-" + datetime.now().strftime('%Y-%m-%d-%H_%M')
         save_path = img_folder + str(cnt) + now + ".png"
+        print('URL: ' + i)
+
+        page = context.new_page()
+        page.goto(i)
+        page.wait_for_load_state()
+        handle_agreement_page(page)
+        hide_html_element(page)
         page.locator("body").screenshot(path = save_path)
         #print('saved in: ' + save_path)
         cnt += 1
+
+def handle_agreement_page(page):
+    checkbox = page.locator("input[type=\"checkbox\"]#agreement").nth(0)
+    nextBtn = page.locator("button#btnNext").nth(0)
+    if checkbox.is_visible():
+        page.evaluate('const btn = document.getElementById("btnNext"); '
+                      'btn.removeAttribute("disabled");')
+        nextBtn.click()
+    page.wait_for_load_state()
+
+def hide_html_element(page):
+    page.evaluate('document.getElementsByClassName("ui-grid-b")[1].style.display = "none";'
+                  'document.getElementsByTagName("legend")[0].style.display = "none";')
 
 with sync_playwright() as playwright:
     run(playwright)
